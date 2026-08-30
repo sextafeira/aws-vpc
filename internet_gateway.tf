8b51bad23ca52e68baea6da7046eaa5a81765a94
sextafeira <bsiludev@outlook.com.br>
2026-08-30 07:17:28 -0300

    feat(aws-vpc): create base infrastructure

---
+++ internet_gateway.tf
@@ -0,0 +1,9 @@
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = format("%s-igw", var.project_name)
+    Environment = var.environment
+    Terraform = "True"
+  }
+}
